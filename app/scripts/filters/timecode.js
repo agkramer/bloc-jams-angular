a4c74a42a9c8e607bd445b00e3d88db18b85074f
Allen Kramer <kramer.alleng@gmail.com>
2016-08-22 09:31:38 -0400

    completed checkpoint 11 | Filters

---
+++ app/scripts/filters/timecode.js
@@ -0,0 +1,16 @@
+(function() {
+    function timecode() {
+        return function(seconds) {
+
+            if (Number.isNaN(seconds)) {
+                return '-:--';
+            }
+
+            return buzz.toTimer(seconds);
+        };
+    }
+
+    angular
+        .module('blocJams')
+        .filter('timecode', timecode);
+})();
